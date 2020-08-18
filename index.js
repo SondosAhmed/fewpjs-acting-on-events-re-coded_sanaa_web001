01c630cf08548d9dd978105dfe7a4dd370d29b8e
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-18 19:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,24 @@ function moveDodgerLeft() {
   }
 }
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(leftNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
-  else if (e.key === "ArrowRight") {
+  
+});
+
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
+  
 });
